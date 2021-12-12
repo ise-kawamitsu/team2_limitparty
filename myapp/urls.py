76e0c1358e43cee923a23c8013666d9188e0f789
ise-kawamitsu <gr_crew@icloud.com>
2021-12-12 18:46:26 +0900

    limitparty

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,14 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.hello, name='hello'),
-#     path('', views.title, name='title'),
-#     path('top/', views.top, name='top'),
-#     path('freever/top/', views.index, name='f_top'),
-#     path('paidver/top/', views.ptop, name='p_top'),
-#     path('paidver/signup/', views.signup, name='signup'),
-#     path('paidver/login/', views.signin, name='login'),
-#     path('paidver/logout/', views.logout_view, name='logout'),
-#     path('game/freequiz/', views.f_quiz, name='f_quiz'),
-#     path('game/paidquiz/', views.p_quiz, name='p_quiz'),
+    path('', views.title, name='title'),
+    path('top/', views.top, name='top'),
+    path('freever/top/', views.index, name='f_top'),
+    path('paidver/top/', views.ptop, name='p_top'),
+    path('paidver/signup/', views.signup, name='signup'),
+    path('paidver/login/', views.signin, name='login'),
+    path('paidver/logout/', views.logout_view, name='logout'),
+    path('game/freequiz/', views.f_quiz, name='f_quiz'),
+    path('game/paidquiz/', views.p_quiz, name='p_quiz'),
 ]
