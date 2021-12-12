197201d0ad545722abca24350c0f48d33a2a9f9b
ise-kawamitsu <gr_crew@icloud.com>
2021-12-12 19:12:00 +0900

    b

--- herokutest/settings.py
+++ herokutest/settings.py
@@ -19,7 +19,7 @@ try:
 except ImportError:
     pass
 
-ALLOWED_HOSTS = ['afternoon-beach-94662.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'afternoon-beach-94662.herokuapp.com']
 
 
 # Application definition
@@ -118,9 +118,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+# ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 if not DEBUG:
